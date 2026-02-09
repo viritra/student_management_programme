8373fed4af89c704c38c1d76ead73ca5584c655e
viritra <ritik08092004@gmail.com>
2026-02-09 21:16:32 +0530

    Update Management_System.c

--- Management_System.c
+++ Management_System.c
@@ -427,28 +427,3 @@ void delstu(int* roll)
         return;
     }
 }
-
-/*  --Discription
-FEATURES TO IMPLEMENT:
-1. Add a new student record
-2. Display all student records
-3. Search for a student by roll number
-4. Update student details
-5. Delete a student record
-6. Save and load data using files
-SAMPLE MENU: 
-1. Add Student
-2. Display Students
-3. Search Student
-4. Update Student
-5. Delete Student
-6. Exit
-EACH STUDENT SHOULD HAVE:
-1. Roll Number
-2. Name
-3. Age
-4. Course
-5. Marks
-*/
-/*Improvements (IMP): Display function- sorting, scanf()- check for invalid inputs after all scanf(), enum for menu, UI clarity
-optimize delstu() by student[j]=student[j+1] instead of student[j].roll = student[j+1].roll; strcpy(student[j].name, student[j+1].name);*/
